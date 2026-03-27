a22609e7bd3508687fa5ecb50cbb3b49c9f2d17a
SequentialCode <harishakulkarni@gmail.com>
2026-03-27 18:48:47 +0530

    switch case version looks more structured, hence easier to understand. It is easy to scale too as you just have to add one more case and performancewise it is more efficient.

--- ExperimentsWithIf/ExperimentsWithIf.cpp
+++ ExperimentsWithIf/ExperimentsWithIf.cpp
@@ -17,34 +17,44 @@ int main()
 	constexpr double yen_to_dollars = 0.0063;
 	constexpr double kroner_to_dollars = 0.10;
 	constexpr double pounds_to_dollars = 1.33;
-	std::cout << "Please enter number of yens(y)/kroner(k)/pounds(p) followed by the currency (y or k or p):\n";
+	constexpr double francs_to_dollars = 1.25;
+	std::cout << "Please enter number of yens(y)/kroner(k)/pounds(p)/francs(f) followed by the currency (y or k or p or f):\n";
+
 	if(!(std::cin >> money >> unit))
 	{
 		std::cerr << "Invalid input format.";
 		return 1;
 	}
-	if(unit == 'y')
+
+	switch(unit)
 	{
-		singular_input = " yen";
-		plural_input = " yens";
-		converted_money = money*yen_to_dollars;
-	} else if(unit == 'k')
-	{
-		singular_input = " kroner";
-		plural_input = " kroners";
-		converted_money = money * kroner_to_dollars;
-	} else if(unit == 'p')
-	{
-		singular_input = " pound";
-		plural_input = " pounds";
-		converted_money = money * pounds_to_dollars;
+		case 'y':
+			singular_input = " yen";
+			plural_input = " yens";
+			converted_money = money*yen_to_dollars;
+		break;
+		case 'k':
+			singular_input = " kroner";
+			plural_input = " kroners";
+			converted_money = money * kroner_to_dollars;
+		break;
+		case 'p':
+			singular_input = " pound";
+			plural_input = " pounds";
+			converted_money = money * pounds_to_dollars;
+		break;
+		case 'f':
+			singular_input = " frank";
+			plural_input = " franks";
+			converted_money = money * francs_to_dollars;		
+		break;
+		default:
+			std::cerr << "Sorry, I don't recognize that unit - " << unit << "!\n";
+	     		return 1;
+		break;
 	}
-	else
-	{
-	     std::cerr << "Sorry, I don't recognize that unit - " << unit << '\n';
-	     return 1;
-	}
+
 	std::cout << money << GetUnit(money, singular_input, plural_input) << " == " << converted_money << GetUnit(converted_money, " dollar", " dollars");
-	
+
 	return 0;	
 }
