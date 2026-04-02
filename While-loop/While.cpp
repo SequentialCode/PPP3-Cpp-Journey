67c31829768fbe99ae01454e3d1923739587ffe5
SequentialCode <harishakulkarni@gmail.com>
2026-04-02 19:02:33 +0530

    augment changes for double

--- While-loop/While.cpp
+++ While-loop/While.cpp
@@ -1,15 +1,33 @@
 /*
-*Write a program that consists of a while-loop that (each time around the loop) reads in two ints and then prints them. Exit the program when a terminating '|' is entered.
+*[1] Write a program that consists of a while-loop that (each time around the loop) reads in two ints and then prints them. Exit the program when a terminating '|' is entered.
+	[2] Change the program to write out the smaller value is: followed by the smaller of the numbers and the larger value is: followed by the larger value.
+	[3] Augment the program so that it writes the line the numbers are equal (only) if they are equal.
+	[4] Change the program so that it uses doubles instead of ints.
 */
 
 import std;
 
 int main()
 {
-	int i{}, j{};
-	while (std::cout << "Enter two integers (or '|' to quit): " && std::cin >> i >> j)
+	double i{}, j{};
+	while (std::cout << "Enter two floating-point numbers (or '|' to quit): " && std::cin >> i >> j)
 	{
-		std::cout << i << '\t' << j << '\n';
+		if (i == j)
+			std::cout << "the numbers are equal\n";
+		else
+		{
+			if (i < j)
+			{
+				std::cout << "the smaller value is: " << i << '\n';
+				std::cout << "the larger value is: " << j << '\n';
+			}
+			else
+			{
+				std::cout << "the smaller value is: " << j << '\n';
+				std::cout << "the larger value is: " << i << '\n';
+			}
+		}
+
 	}
 	return 0;
 }
