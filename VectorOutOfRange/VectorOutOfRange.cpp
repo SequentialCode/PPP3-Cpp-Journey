aa9c855e0cc1912dabb2ec74e2c52117a124f5b8
SequentialCode <harishakulkarni@gmail.com>
2026-04-19 10:25:51 +0530

    handle the exception that at operator throws

--- VectorOutOfRange/VectorOutOfRange.cpp
+++ VectorOutOfRange/VectorOutOfRange.cpp
@@ -2,10 +2,23 @@ import std;
 
 int main()
 {
-	std::vector<int> data;
-	int input{};
-	while(std::cin >> input)
-		 data.push_back(input);
-	for (int i = 0; i <= data.size(); ++i) //vector subscript operator does not perform bounds checking, so this will cause undefined behavior when i == data.size()
-		std::cout << "index - " << i << " | value - " << data[i] << '\n';
+	try{
+		std::vector<int> data;
+		int input{};
+		while(std::cin >> input)
+			 data.push_back(input);
+		for (int i = 0; i <= data.size(); ++i) 
+			//std::cout << "index - " << i << " | value - " << data[i] << '\n'; // this will cause undefined behavior when i == data.size()
+			std::cout << "index - " << i << " | value - " << data.at(i) << '\n'; // this will throw an out_of_range exception when i == data.size()
+	}
+	catch(std::out_of_range e)
+	{
+		std::cerr << "vector subscriptor accessing out of range elements";
+		return 1;
+	}
+	catch(...)
+	{
+		std::cerr << "Something unknown went wrong";
+		return 2;
+	}
 }
