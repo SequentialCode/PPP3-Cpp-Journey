b36ef6cc31204f93fd18a0a7b9bfdce0d56d8b84
SequentialCode <harishakulkarni@gmail.com>
2026-06-22 15:40:09 +0530

    protected constructor

--- initializer_list/Source.cpp
+++ initializer_list/Source.cpp
@@ -8,27 +8,29 @@ struct Point
 
 class Shape 
 {
-   public:
+   protected:
    //parameterized constrcutor with default arg
     Shape(std::initializer_list<Point> lst = {}) : vertices {lst}
     {
       std::cout << "Created a shape with " << vertices.size() << " vertices.\n";
     }
-    
     private:
     std::vector<Point> vertices;
 };
 
+class Circle : public Shape
+{
+   public:
+   Circle(int id)// Call the base class constructor with an empty initializer list
+   {
+      std::cout << "Created a circle.\n";
+   }
+ 
+};
+
 int main()
 {
-   Shape d;
-   Shape d1{};
-   Shape point {{0,0}};
-   Shape line {{0,0}, {5, 5}};
-   Shape triangle { {0,0}, {4,0}, {2, 3}};
-   Shape square { {0,0}, {4,0}, {4,4}, {0,4} };
-   Shape pentagon { {2,4}, {4,2}, {3,0}, {1,0}, {0,2}};
-   Shape hexagon { {1,3}, {3,3}, {4,1}, {3,-1}, {1,-1}, {0,1}};
-   Shape octagon { {1,3}, {2,3}, {3,2}, {3,1}, {2,0}, {1,0}, {0,1}, {0,2}};
+   Circle c{1};
+   Shape s; //locked away shape so that only using derived classes you create shapes
 }
 
